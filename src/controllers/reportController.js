1f0e78037edfe7ed96d0de6386e6e7f7afa35f0f
anandmatkar <139226044+anandmatkar@users.noreply.github.com>
2023-10-04 11:05:00 +0530

    changes in variable name

--- src/controllers/reportController.js
+++ src/controllers/reportController.js
@@ -10,7 +10,7 @@ const { welcomeEmail2, notificationMailToAdmin, resetPasswordMail, sendProjectNo
 module.exports.createReport = async (req, res) => {
     try {
         let { id, position } = req.user
-        let { projectID, date, description, attachements } = req.body
+        let { projectID, date, description, attachement } = req.body
         await connection.query("BEGIN")
         let s1 = dbScript(db_sql['Q27'], { var1: id })
         let findTechnician = await connection.query(s1)
@@ -18,11 +18,10 @@ module.exports.createReport = async (req, res) => {
             let s2 = dbScript(db_sql['Q48'], { var1: projectID, var2: id, var3: findTechnician.rows[0].manager_id, var4: date, var5: description })
             let createReport = await connection.query(s2)
             if (createReport.rowCount > 0) {
-                if (attachements.length > 0) {
-                    for (let files of attachements) {
+                if (attachement.length > 0) {
+                    for (let files of attachement) {
                         let s3 = dbScript(db_sql['Q61'], { var1: projectID, var2: id, var3: files.path, var4: files.mimetype, var5: files.size, var6 : createReport.rows[0].id })
                         let createReportAttach = await connection.query(s3)
-                        console.log(createReportAttach.rows, "1111111111");
                     }
                 }
                 await connection.query("COMMIT")
