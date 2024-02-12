abc619a4d6562523d2d5bf7443d4fbd9e5e775a6
anandmatkar <anandmatkar123#>
2024-02-12 10:20:23 +0530

    changes in connection file

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,13 +1,13 @@
 const { Pool } = require('pg')
 var connection = new Pool({
     host: "localhost",
-    user:"postgres",
-    password:"Developer123#",
+    user: "postgres",
+    password: "FseWorks1234!",
     database: "fse_project",
     charset: 'utf8mb4'
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) throw err;
     console.log("database Connected successfully!");
 })
